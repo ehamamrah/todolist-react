328d9e0fbee6a5cabd676ff572e7c402bcd015e3
ehamamrah <elayan.hr@gmail.com>
2020-10-26 03:20:06 +0300

    More Function & Design

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
 import TodoList from './tasks/ToDoList';
 import uuidv4 from 'uuid/v4';
+import { BsFillTrashFill, BsFileEarmarkPlus } from 'react-icons/bs';
 
 const LOCAL_STORAGE_KEY = 'tasks';
 
@@ -44,19 +45,25 @@ function App() {
 
   return (
     <>
-      <TodoList todos={todos} toggleTask={toggleTask} />
-      <input type="text" ref={taskNameRef} />
-      <button className="btn btn-success" onClick={handleAddTask}>
-        Add Task
-      </button>
-      <button className="btn btn-danger" onClick={handleClearCompleted}>
-        Clear Completed Tasks
-      </button>
-      <div>
-        Pending Tasks
-        <span className="badge badge-primary">
-          { todos.filter(task => !task.complete).length }
-        </span>
+      <div className='container' >
+        <div className='row'>
+          <div className='mx-auto pt-5'>
+            <TodoList todos={todos} toggleTask={toggleTask} />
+            <input type="text" ref={taskNameRef} />
+            <button className="btn btn-success" onClick={handleAddTask}>
+              <BsFileEarmarkPlus /> Create Task
+            </button>
+            <button className="btn btn-danger" onClick={handleClearCompleted}>
+              <BsFillTrashFill /> Clear Completed
+            </button>
+            <div>
+              Pending Tasks
+              <span className="badge badge-primary">
+                { todos.filter(task => !task.complete).length }
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
--- src/tasks/Task.js
+++ src/tasks/Task.js
@@ -4,12 +4,21 @@ export default function Task( { task, toggleTask } ) {
   function handleTaskClick(){
     toggleTask(task.id);
   }
+  function strikeCompleted(task){
+    if (task.complete){
+      return <s>{task.name}</s>;
+    } else {
+      return task.name;
+    }
+  }
   return (
-    <div>
-      <label>
-        <input type='checkbox' checked={task.complete} onChange={handleTaskClick}/>
-        {task.name}
-      </label>
+    <div className="card mb-2">
+      <div className="card-body">
+        <h5 className="card-title">
+          <span className="pr-2"><input type='checkbox' checked={task.complete} onChange={handleTaskClick}/></span>
+          {strikeCompleted(task)}
+        </h5>
+      </div>
     </div>
   )
 }
